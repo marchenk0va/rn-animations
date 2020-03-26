4a42aa32244b0fddef0dd59148e001da181f4251
Mariia Marchenkova <marchewka@Mariias-MacBook-Pro.local>
2020-03-26 17:49:05 +0100

    feat: Added navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>RN Animations</Text>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text style={styles.title}>RN Animations</Text>
+      </View>
+    </NavigationContainer>
   );
 };
 
